44d342a62329483235ac425c3e17ee543ff64b4e
Skyler Jane Clark <skylerjaneclark@gmail.com>
2018-10-23 14:06:46 -0400

    attemps to set up redis

--- public/js/app/getLocation.js
+++ public/js/app/getLocation.js
@@ -3,6 +3,7 @@ var x = $('#demo');
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(saveLocation);
     } else {
         x.html("aaa");
     }
@@ -11,4 +12,15 @@ function showPosition(position) {
     var x = $('#demo');
     x.html("Latitude: " + position.coords.latitude +
         "<br>Longitude: " + position.coords.longitude);
+}
+
+function saveLocation(position){
+    $.ajax({
+        type: "POST",
+        url: "/checkin",
+        data:{
+            "latitude" : position.coords.latitude,
+            "longitude": position.coords.longitude,
+        }
+    });
 }
