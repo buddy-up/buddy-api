ce2610707d19528d6d865e374f6f627b3c2e777c
Skyler Jane Clark <skylerjaneclark@gmail.com>
2018-10-27 04:51:45 -0400

    starts moving towards actual redis geolocation things

--- app/controllers/geoLocation.go
+++ app/controllers/geoLocation.go
@@ -33,19 +33,30 @@ func (c Application) CheckIn (code string) revel.Result {
 	user := c.ViewArgs["user"].(*models.User)
 
 	conn := RedisConnect()
-	latitudeReply, latitudeErr := conn.Do("SET", user.Id.String() + "latitude:" ,  latitude)
-	longitudeReply, longitudeErr := conn.Do("SET", user.Id.String() +"longitude:" , longitude)
-	if latitudeErr != nil{
-		panic(latitudeErr)
+
+	locationSetReply, locationSetErr := conn.Do("GEOADD", "user_locations", latitude, longitude, user.Id)
+
+	if locationSetErr != nil{
+		panic(locationSetErr)
 	}
-	if longitudeErr != nil{
-		panic(longitudeErr)
-	}
-	fmt.Println("GET ", longitudeReply)
-	fmt.Println("GET ", latitudeReply)
+	fmt.Println("GET ", locationSetReply)
 	defer conn.Close()
 
 	fmt.Println(latitude)
 	fmt.Println(longitude)
 	return c.Redirect(Application.Index)
+}
+
+func (c Application) FindNearby (code string) revel.Result {
+	user := c.ViewArgs["user"].(*models.User)
+
+	conn := RedisConnect()
+
+	nums, err := redis.Values(conn.Do("GEORADIUS", user.Id, 15, 37, 200, "km", "WITHCOORD", "WITHDIST"))
+	if(err != nil){
+		fmt.Print(err)
+	}
+	fmt.Println(nums)
+	return c.Redirect(Application.Index)
+
 }
--- public/js/app/getLocation.js
+++ public/js/app/getLocation.js
@@ -4,6 +4,7 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
         navigator.geolocation.getCurrentPosition(saveLocation);
+        navigator.geolocation.getCurrentPosition(findNearMe);
     } else {
         x.html("aaa");
     }
@@ -18,6 +19,17 @@ function saveLocation(position){
     $.ajax({
         type: "POST",
         url: "/checkin",
+        data:{
+            "latitude" : position.coords.latitude,
+            "longitude": position.coords.longitude,
+        }
+    });
+}
+
+function findNearMe(position){
+    $.ajax({
+        type: "POST",
+        url: "/find_nearby",
         data:{
             "latitude" : position.coords.latitude,
             "longitude": position.coords.longitude,
